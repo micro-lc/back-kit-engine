74e60119934d1d2fcd948beeabeeeb1c2f301821
Claudio Benedetti <claudio.benedetti@mia-platform.eu>
2022-11-15 23:42:04 +0100

    Add time-zone field in DateOptions

--- src/schemas/date.ts
+++ src/schemas/date.ts
@@ -1,4 +1,5 @@
 export type DateOptions = {
-  /** Defines how the date should be displayed in Table, Form, ecc. */
+  /** Defines how the date should be displayed in Table | Form | ecc. */
   displayFormat?: string
+  timeZone?: string
 }
