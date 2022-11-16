d3626469c575d64061274692cd2ea38feb486ae4
Claudio Benedetti <claudio.benedetti@mia-platform.eu>
2022-11-16 20:06:08 +0100

    sort option in lookupOptions

--- src/schemas/date.ts
+++ src/schemas/date.ts
@@ -1,5 +1,7 @@
 export type DateOptions = {
   /** Defines how the date should be displayed in Table | Form | ecc. */
   displayFormat?: string
+
+  /** Time zone to use in case data is a numeric value */
   timeZone?: string
 }
